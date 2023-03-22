18abb635d49dfa1a8885b28afc84c7349dacefcd
wyongjae <dnjsdydwo123@naver.com>
2023-03-22 23:17:43 +0900

    Add: clericInfo method in Hero

--- lib/hero/hero.dart
+++ lib/hero/hero.dart
@@ -1,6 +1,17 @@
+import 'package:dart_language_prac/hero/cleric.dart';
+
 class Hero {
   String name;
   int? hp = 50;
 
   Hero({required this.name, this.hp});
+
+  Cleric cleric = Cleric(name: '아서스', hp: 5, mp: 5);
+
+  void clericInfo() {
+    cleric.maxHp;
+    cleric.maxMp;
+    cleric.pray(3);
+    cleric.selfAid();
+  }
 }
